70ab1c0bfee929ae7f6a4db40cf9f6c54eccea68
Annie Chen <annie.chen@yale.edu>
2016-07-25 14:54:43 +0000

    reordering of unit 1

---
+++ Unit1/2Syntax/syntax-disaster.c
@@ -0,0 +1,31 @@
+/**
+ * syntax-disaster.c
+ * 
+ * NOTE: this code is not meant to compile 
+ * 
+ * Can be used as a worksheet or in class exercise to fix the syntax errors from the top down. 
+ * This worksheet was created when Syntax was the last unit in the module
+ */    
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf(hello, world\n")
+    
+    cat = 5;
+    
+    while (cat = 5)
+    {
+        cat 3++;
+    }
+    
+    string catSound;
+    
+    "meow" = catSound;
+    
+    for (int i = 0, i < 5; i++)
+    {
+        printf("Counting to 5...\n");
+    }
+}
