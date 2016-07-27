b7a59c6c641e44cd7a700d84a930e55bbb5c222c
Annie Chen <annie.chen@yale.edu>
2016-07-27 19:50:50 +0000

    clarifying typecasting

--- Unit2/5Libraries/include-libraries-solution.c
+++ Unit2/5Libraries/include-libraries-solution.c
@@ -12,9 +12,7 @@
  int main(void)
  {
      printf("Hello World\n");
-     
      int numCharacters = strlen("Hello World");
-     
      printf("Hello World has %d letters in it\n", numCharacters);
-     printf("2 squared is %f\n", pow(2,2));
+     printf("2 squared is %f\n", pow(2.0,2.0));
  }
